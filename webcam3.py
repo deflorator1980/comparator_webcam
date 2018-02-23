547ba71cc784ea18ad3c9ad044ff2d921281769a
Your Name <you@example.com>
2018-02-23 18:54:32 +0300

    requrements

--- webcam3.py
+++ webcam3.py
@@ -3,16 +3,6 @@ import cv2
 import pickle
 import postgresql
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 db = postgresql.open('pq://postgres:postgres@localhost:5432/comparator3')
